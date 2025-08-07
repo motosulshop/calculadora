90e56a5465c032b7d22da1be71409d6b254bcf65
Junior <ejcj1980@msn.com>
2025-08-07 11:29:17 -0300

    ok

--- backendcalculadora/salvalead.php
+++ backendcalculadora/salvalead.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 // Configurações do banco
 $host = 'localhost';
 $db = 'motosulshop';
